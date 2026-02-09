5da6e5e9b2501d508a2c2bf6007da73665e6d066
Rory Jarrard <roryjarrard@gmail.com>
2026-02-09 11:22:50 -0700

    add handlebars, basic views

--- meadowlark.js
+++ meadowlark.js
@@ -1,32 +1,35 @@
+const path = require('path');
 const express = require('express');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
+const viewsPath = path.join(__dirname, 'views');
+
+// configure Handlebars view engine
+app.engine('handlebars', engine({
+    extname: '.handlebars',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', viewsPath);
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
-});
+app.get('/', (req, res) => res.render('home'));
 
-app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
-});
+app.get('/about', (req, res) => res.render('about'));
 
 // custom 404 page
 app.use((req, res) => {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
-});
+// app.use((err, req, res, next) => {
+//     console.error(err.message);
+//     res.status(500);
+//     res.render('500');
+// });
 
 app.listen(port, () => console.log(`Meadowlark app listening on port ${port}...`));
